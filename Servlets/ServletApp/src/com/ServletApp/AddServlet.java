a42ac56d428169d176dcdaa3cd13205245fbb281
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-05-12 17:32:48 +0530

    RequestDispatcher

--- Servlets/ServletApp/src/com/ServletApp/AddServlet.java
+++ Servlets/ServletApp/src/com/ServletApp/AddServlet.java
@@ -2,18 +2,26 @@ package com.ServletApp;
 
 import java.io.PrintWriter;
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
 		
-		PrintWriter out = res.getWriter();
-		out.println("The result is: " + k);
+		// Request Dispatcher and Redirect
+		req.setAttribute("k", k);
+		RequestDispatcher rd = req.getRequestDispatcher("/sq");
+		rd.forward(req, res);
+		
+//		PrintWriter out = res.getWriter();
+//		out.println("The result is: " + k);
 	}
 }

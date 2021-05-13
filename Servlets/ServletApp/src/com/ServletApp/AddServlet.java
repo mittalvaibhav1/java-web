65727fed01dc4507216b6dc0970554daaa425771
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-05-13 19:47:01 +0530

    Sessions: HttpSession

--- Servlets/ServletApp/src/com/ServletApp/AddServlet.java
+++ Servlets/ServletApp/src/com/ServletApp/AddServlet.java
@@ -8,22 +8,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i + j;
+
+//		Session Management
+		HttpSession session = req.getSession();
+		session.setAttribute("k", k);
+		res.sendRedirect("sq");
 		
-//		Request Dispatcher
-//		req.setAttribute("k", k);
-//		RequestDispatcher rd = req.getRequestDispatcher("/sq");
-//		rd.forward(req, res);
-		
-//		Redirect
-		res.sendRedirect("sq?k="+k);
-		
-//		PrintWriter out = res.getWriter();
-//		out.println("The result is: " + k);
 	}
 }
--- Servlets/ServletApp/src/com/ServletApp/SqServlet.java
+++ Servlets/ServletApp/src/com/ServletApp/SqServlet.java
@@ -6,11 +6,14 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 //		int k = (int) req.getAttribute("k");
-		int k = Integer.parseInt(req.getParameter("k"));
+		HttpSession session = req.getSession();
+		int k = (int) session.getAttribute("k");
+		session.removeAttribute("k");
 		PrintWriter out = res.getWriter();
 		out.println(k + " is SQ");
 	}
